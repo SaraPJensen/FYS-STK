2afd57b99b08a1dd61ba341f8f960c0568db9fb6
SaraPJensen <sarapje@ifi.uio.no>
2021-09-23 10:40:27 +0200

    import

--- Project1/scalers.py
+++ Project1/scalers.py
@@ -4,27 +4,39 @@ Created on Wed Sep 22 10:41:27 2021
 
 @author: haako
 """
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+from random import random, seed
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.utils import resample
+
 
 def scalerStandard(X_train, X_test, z_train, z_test):
     scaler = StandardScaler()
     scaler.fit(X_train)
-    
+
     X_train_scaled = scaler.transform(X_train)
     X_test_scaled = scaler.transform(X_test)
 
     #scale the response variable
     z_train_scaled = (z_train - np.mean(z_train))/np.std(z_train)
     z_test_scaled = (z_test - np.mean(z_train))/np.std(z_train)
-    
+
     return X_train_scaled, X_test_scaled, z_train_scaled, z_test_scaled
 
 def scalerMinMax(X_train, X_test, z_train, z_test):
     scaler = MinMaxScaler()
     scaler.fit(X_train)
-    
+
     X_train_scaled = scaler.transform(X_train)
     X_test_scaled = scaler.transform(X_test)
-    
+
     #Scale the response variable
     z_train = z_train.reshape((-1,1))
     z_test = z_train.reshape((-1,1))
@@ -34,14 +46,14 @@ def scalerMinMax(X_train, X_test, z_train, z_test):
     scaler3.fit(z_test)
     z_train_scaled = scaler2.transform(z_train)
     z_test_scaled = scaler3.transform(z_test)
-    
+
     return X_train_scaled, X_test_scaled, z_train_scaled.flatten(), z_test_scaled.flatten()
 
 def scalerNormalizer(X_train, X_test, z_train, z_test):
     scaler = Normalizer().fit(X_train)
     X_train_scaled = scaler.transform(X_train)
     X_test_scaled = scaler.transform(X_test)
-    
+
     #Scale the response variable
     z_train = z_train.reshape((1,-1))
     z_test = z_test.reshape((1,-1))
@@ -49,14 +61,14 @@ def scalerNormalizer(X_train, X_test, z_train, z_test):
     z_train_scaled = scaler.transform(z_train)
     scaler = Normalizer().fit(z_test)
     z_test_scaled = scaler.transform(z_test)
-    
+
     return X_train_scaled, X_test_scaled, z_train_scaled.flatten(), z_test_scaled.flatten()
 
 def scalerRobust(X_train, X_test, z_train, z_test):
     scaler = RobustScaler().fit(X_train)
     X_train_scaled = scaler.transform(X_train)
     X_test_scaled = scaler.transform(X_test)
-    
+
     #Scale the response variable
     z_train = z_train.reshape((-1,1))
     z_test = z_test.reshape((-1,1))
@@ -64,5 +76,5 @@ def scalerRobust(X_train, X_test, z_train, z_test):
     z_train_scaled = scaler.transform(z_train)
     scaler = RobustScaler().fit(z_test)
     z_test_scaled = scaler.transform(z_test)
-    
+
     return X_train_scaled, X_test_scaled, z_train_scaled.flatten(), z_test_scaled.flatten()
