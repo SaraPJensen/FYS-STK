143e5280d5ac2ef116e525309c686444fc6493e2
daniejaa <daniejaa>
2021-10-02 16:03:16 +0200

    further work

--- Project1/skcompare.py
+++ Project1/skcompare.py
@@ -1,5 +1,6 @@
 from scalers import *
 from functions import *
+from sklearn.preprocessing import scale
 
 MSE = mean_squared_error
 R2 = r2_score
@@ -12,7 +13,7 @@ n = 20
 x = np.sort((np.random.rand(n)))        # Equal to np.random.uniform(0, 1, n)
 y = np.sort((np.random.rand(n)))
 x, y = np.meshgrid(x, y)
-z = FrankeFunction(x, y) + 0.03*np.random.randn(n, n)
+z = FrankeFunction(x, y) + 0.1*np.random.randn(n, n)
 
 x_flat = np.ravel(x)
 y_flat = np.ravel(y)
@@ -25,12 +26,12 @@ k_fold = 5
 reg_method = "Ridge"
 n_lambda = 200
 
-manual_mse = CrossVal(x_flat, y_flat, z_flat, scaler, poly, k_fold, reg_method, n_lambda)
+#manual_mse = CrossVal(x_flat, y_flat, z_flat, scaler, poly, k_fold, reg_method, n_lambda)
 
 #print(np.shape(manual_mse))
 
 def OLS_compare(x, y, z, scaler, lamb, poly):
-    # Our code
+    # Our function
     X = design_matrix(x, y, poly)
     X_train, X_test, z_train, z_test = train_test_split(X, z, test_size = 0.2)
     z_train_scaled, z_test_scaled, z_predict, z_model = OLS_Ridge(X_train, X_test, z_train,
@@ -39,12 +40,25 @@ def OLS_compare(x, y, z, scaler, lamb, poly):
     manual_mse = MSE(z_test_scaled, z_predict)
     
     # Scikit-learn method
-    #X_train_scaled, X_test_scaled, z_train_scaled, z_test_scaled = StandardScaler()<++>
-    model = LinearRegression(fit_intercept = False)      # Fit intercept
-    model.fit(X_train, z_train)
+    model = LinearRegression(fit_intercept = True)      # Fit intercept
+    '''
+    scaler = StandardScaler()
+    X_train_sc = scaler.fit_transform(X_train)
+    X_test_sc = scaler.fit_transform(X_test)
+    z_train_sc = scaler.fit_transform(z_train.reshape(-1, 1))
+    z_test_sc = scaler.fit_transform(z_test.reshape(-1, 1))
+    X_train_sc = scale(X_train)
+    X_test_sc = scale(X_test)
+    z_train_sc = scale(z_train.reshape(-1, 1))
+    z_test_sc = scale(z_test.reshape(-1, 1))
+    '''
+    #print(np.shape(X_train_sc))
+    #print(np.shape(z_train_sc))
+    model.fit(X_train, z_train)     # Fitter modellen uten skalering
     zpred = model.predict(X_test)
 
-    sk_mse = MSE(z_test, zpred)     # Funker jævlig bra for z_test (uskalert), fordi modellen er fittet mot uskalert data.
+    sk_mse = MSE(z_test, zpred)     # Funker jævlig bra for z_test (uskalert), fordi modellen er fittet mot uskalert data. Funker SHIT når vi fitter mot skalert
+    # Hvorfor funker det ikke :'(
 
     print("Manuell: ",manual_mse)
     print("sk: ", sk_mse)
