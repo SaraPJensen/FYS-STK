7324dc02ea74d6f8cb01082e75251a48584b75b6
daniejaa <daniejaa>
2021-10-20 11:06:31 +0200

    initialze proj2, temp 1a work

---
+++ Project2/regclass.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class Regression:
+    def __init__(self, x, y, target, maxpoly):
+        xflat = x.ravel()
+        yflat = y.ravel()
+
+    def beta(self, lamb):
+        pass
+
+    def OLS(self):
+        pass
+
+    def ridge(self):
+        pass
+
+    def GD(self): # Gradient descent
+        pass
+
+    def SGD(self): # Stochastic Gradient descent
+        pass
