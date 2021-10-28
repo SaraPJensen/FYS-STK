8da2d9a904a6bd1baf81976747a14181677f26dd
SaraPJensen <sarapje@ifi.uio.no>
2021-10-28 15:20:56 +0200

    further work on SGD

--- Project2/SGD.py
+++ Project2/SGD.py
@@ -34,7 +34,7 @@ def design_matrix(x_flat, y_flat, poly):
 
 np.random.seed(2018)
 
-n_dpoints = 20
+n_dpoints = 40
 
 noise = 0
 
@@ -58,12 +58,13 @@ X_train_tot, X_test_tot, z_train, z_test = train_test_split(X, z_flat, test_size
 
 
 
-poly = 5
+poly = 8
 
 features = int((poly + 1) * (poly + 2) / 2)
 
 
 X_train = X_train_tot[:, :features]
+
 X_test = X_test_tot[:, :features]
 
 #To find the optimal eta, find the eigenvalues of XTX
@@ -71,34 +72,123 @@ matrix = X_train.T @ X_train
 #print(np.shape(matrix))
 eig_vals, eig_vecs = np.linalg.eig(matrix)
 
-#Our own Stochastic gradient descent without minibatches
+#Optimal learning rate is less than 1/max(eigvals)
+print("Maximum learning rate: ", 1/max(eig_vals))
+
+lamb = 0
+
+
+#-------------------------------
+#Regular gradient Descent
+#-------------------------------
 #Necessary variables for SGD
 max_iter = 50
-penalty = None
 theta = np.random.randn(features, 1)
-#eta = 0.025
-eta = 1 / np.max(eig_vals)
+#eta = 0.0025
+eta_GD = (1 / np.max(eig_vals))
+
 n_iterations = 100000
 
 z_train = z_train.reshape(-1, 1)
 
+#Define the cost function to be able to take the gradient using autograd
+def cost_func(X_train, theta, z_train):
+    return (1/len(X_train[:, 0]))*((X_train @ theta)-z_train)**2      #np.sum(((X_train @ theta)-z_train)**2)
+
+
 
 for iter in range(n_iterations):
-    gradients = 2.0/ n_dpoints * X_train.T @ ((X_train @ theta)-z_train)
+
+    gradients = X_train.T @ ((X_train @ theta)-z_train) + lamb * theta   #replace this with autograd
+    #Should this be n_dpoints???   Removed at the start:  2.0/ n_dpoints *
+    #derivative = egrad(cost_func, 1)
+    #gradients = derivative(X_train, theta, z_train)   #take the derivative w.r.t. theta
 
     if abs(sum(gradients)) <= 0.00001:
-        print(sum(gradients))
-        print("Iterations: ", iter)
         break
+    theta -= eta_GD*gradients
 
-    theta -= eta*gradients
-
-
+#print("GD theta: ", theta)
 
 z_predict = X_test @ theta
 z_model = X_train @ theta
 
 
+
+print("Gradient descent")
+mse_train = mean_squared_error(z_train, z_model)
+print(f"MSE, train: {mse_train:.5}")
+print('')
+mse_test = mean_squared_error(z_test, z_predict)
+print(f"MSE, test: {mse_test:.5}")
+print('')
+
+r2_train = r2_score(z_train, z_model)
+print(f"R2, train: {r2_train:.5}")
+print('')
+r2_test = r2_score(z_test, z_predict)
+print(f"R2, test: {r2_test:.5}")
+print('')
+
+
+
+#-----------------------------
+#Stochastic Gradient Descent
+#-----------------------------
+
+#Parameters
+epochs = 1000
+batch_size = 5
+batches = int(len(X_train[:, 0])/batch_size)
+eta = 0.0025   #need to add some algorithm to scale the learning rate
+theta = np.random.randn(features, 1)
+
+
+for e in range (epochs):
+    for b in range (batches):
+        indices = np.random.randint(0, high = len(X_train[:, 0]), size = batch_size)
+        X_b = X_train[indices]
+        z_b = z_train[indices]
+        gradient = X_b.T @ ((X_b @ theta)-z_b) + lamb * theta
+
+        if abs(sum(gradient)) <= 0.00001:
+            break
+
+        theta -= eta*gradient
+
+
+#print("SGD theta: ", theta)
+
+z_predict = X_test @ theta
+z_model = X_train @ theta
+
+
+print("Stochastic gradient descent")
+
+mse_train = mean_squared_error(z_train, z_model)
+print(f"MSE, train: {mse_train:.5}")
+print('')
+mse_test = mean_squared_error(z_test, z_predict)
+print(f"MSE, test: {mse_test:.5}")
+print('')
+
+r2_train = r2_score(z_train, z_model)
+print(f"R2, train: {r2_train:.5}")
+print('')
+r2_test = r2_score(z_test, z_predict)
+print(f"R2, test: {r2_test:.5}")
+print('')
+
+
+#----------------------
+#Sklearn SGD    - gives worse results - very strange...
+#----------------------
+SGDreg = SGDRegressor(max_iter = n_iterations, penalty = None, eta0 = eta, shuffle = True)
+SGDreg.fit(X_train, np.ravel(z_train))
+z_model = SGDreg.predict(X_train)
+z_predict = SGDreg.predict(X_test)
+
+print("Sklearn SGD")
 
 mse_train = mean_squared_error(z_train, z_model)
 print(f"MSE, train: {mse_train:.5}")
@@ -118,8 +208,9 @@ print('')
 
 
 
-
 '''
+
+
 n_epochs = 50
 M = 5   #size of each minibatch
 m = int(n/M) #number of minibatches
--- Project2/regclass.py
+++ Project2/regclass.py
@@ -1,17 +1,17 @@
 import numpy as np
-from sklearn.model_selection import train_test_split 
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, Normalizer# scalerStandard, scalerMinMax, scalerMean, scalerRobust
 
 def design_matrix(x_flat, y_flat, poly):
     l = int((poly+1)*(poly+2)/2)                # Number of elements in beta
     X = np.ones((len(x_flat),l))
-  
+
     for i in range(0, poly+1):
         q = int((i)*(i+1)/2)
-  
+
         for k in range(i+1):
             X[:,q+k] = (x_flat**(i-k))*(y_flat**k)
-  
+
     return X
 
 class Regression:
@@ -47,30 +47,109 @@ class Regression:
             print("invalid argument in scale() function.\
                     Possible args: Standard, MinMax, Mean, Normalizer")
             quit(1)
-                    
+
 
     def beta(self, lamb):
         I = np.eye( len( X_train[0, :]) )
         if self.scaled == False:
             beta = np.linalg.pinv(self.X_train.T @ self.X_train + lamb*I) @\
                     self.X_train.T @ self.z_train
+
         elif self.scaled == True:
             beta = np.linalg.pinv(self.X_train_scaled.T @ self.X_train_scaled + lamb*I) @\
                     self.X_train_scaled.T @ self.z_train_scaled
 
         return beta
 
-    def OLS(self, scale_arg = "none", scale = True):
+    def OLS_Ridge(self, scale_arg = "none", scale = True, lamb):
         scale(scale_arg)
 
+        beta = beta(self, lamb)
+        z_model = self.X_train_scaled @ beta
+        z_predict = self.X_test_scaled @ beta
 
+
+
+    def Ridge(self):
         pass
 
-    def ridge(self):
-        pass
+    def GD(self, n_iterations, eta): # Gradient descent
+        theta = np.random.randn(features, 1)
+        #eta = 0.0025
+        eta_GD = (1 / np.max(eig_vals))
 
-    def GD(self): # Gradient descent
-        pass
+        n_iterations = 100000
 
-    def SGD(self): # Stochastic Gradient descent
-        pass
+        z_train = self.z_train.reshape(-1, 1)
+
+        #Define the cost function to be able to take the gradient using autograd
+        def cost_func(X_train, theta, z_train):
+            return (1/len(X_train[:, 0]))*((X_train @ theta)-z_train)**2      #np.sum(((X_train @ theta)-z_train)**2)
+
+
+
+        for iter in range(n_iterations):
+
+            gradients = self.X_train.T @ ((self.X_train @ theta)-self.z_train) + lamb * theta   #replace this with autograd
+            #Should this be n_dpoints???   Removed at the start:  2.0/ n_dpoints *
+            #derivative = egrad(cost_func, 1)
+            #gradients = derivative(X_train, theta, z_train)   #take the derivative w.r.t. theta
+
+            if abs(sum(gradients)) <= 0.00001:
+                break
+            theta -= eta_GD*gradients
+
+        #print("GD theta: ", theta)
+
+        z_predict = self.X_test @ theta
+        z_model = self.X_train @ theta
+
+        error_estimate(self, self.z_train, self.z_test, z_model, z_predict)
+
+
+
+    def SGD(self, epochs, batch_size, eta): # Stochastic Gradient descent
+        epochs = 1000
+        batch_size = 5
+        batches = int(len(X_train[:, 0])/batch_size)
+        eta = 0.0025   #need to add some algorithm to scale the learning rate
+        theta = np.random.randn(features, 1)
+
+        z_train = self.z_train.reshape(-1, 1)
+
+        for e in range (epochs):
+            for b in range (batches):
+                indices = np.random.randint(0, high = len(self.X_train[:, 0]), size = batch_size)
+                X_b = self.X_train[indices]
+                z_b = self.z_train[indices]
+                gradient = X_b.T @ ((X_b @ theta)-z_b) + lamb * theta
+
+                if abs(sum(gradient)) <= 0.00001:
+                    break
+
+                theta -= eta*gradient
+
+
+        #print("SGD theta: ", theta)
+
+        z_predict = self.X_test @ theta
+        z_model = self.X_train @ theta
+
+        error_estimate(self, self.z_train, self.z_test, z_model, z_predict)
+
+
+
+    def error_estimate(self, z_train, z_test, z_model, z_predict):
+        mse_train = mean_squared_error(z_train, z_model)
+        print(f"MSE, train: {mse_train:.5}")
+        print('')
+        mse_test = mean_squared_error(z_test, z_predict)
+        print(f"MSE, test: {mse_test:.5}")
+        print('')
+
+        r2_train = r2_score(z_train, z_model)
+        print(f"R2, train: {r2_train:.5}")
+        print('')
+        r2_test = r2_score(z_test, z_predict)
+        print(f"R2, test: {r2_test:.5}")
+        print('')
