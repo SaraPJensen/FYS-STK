2d2d9cf8db38b5b9e335b14c59b64b5e3596dcef
SaraPJensen <sarapje@ifi.uio.no>
2021-11-02 13:53:51 +0100

    I think this is working as it should now

--- Project2/neural.py
+++ Project2/neural.py
@@ -30,7 +30,7 @@ def relu(z):
     else:
         return z
 
-def softmax(z):
+def leaky_relu(z):
     pass
 
 
@@ -39,19 +39,18 @@ def softmax(z):
 #-----------------------------------------------
 
 class NeuralNetwork:
-    def __init__(self, X_train, z_train, X_test, z_test, n_hidden_nodes, n_hidden_layers, epochs, batch_size, eta, lamb, activation_func = "sigmoid", cost_func = "MSE"):
+    def __init__(self, X_train, z_train, X_test, z_test, hidden_nodes, epochs, batch_size, eta, lamb, activation_func = "sigmoid", cost_func = "MSE"):
 
         self.X_train = X_train
         self.z_train = z_train
         self.X_test = X_test
         self.z_test = z_test
 
-        self.n_hidden_nodes = n_hidden_nodes
-        self.n_hidden_layers = n_hidden_layers  #number of hidden layers
+        self.hidden_nodes = hidden_nodes
+
         self.n_features = len(self.X_train[0,:])
         self.n_datapoints = len(self.X_train[:, 0])
 
-        #self.n_output_nodes = n_output_nodes   #Don't need this
         self.epochs = epochs
         self.batch_size = batch_size
 
@@ -64,39 +63,30 @@ class NeuralNetwork:
         self.layers = []  #list of layer-objects, containing all the hidden layers plus the input and output layers
 
 
-
         #------------------------------------------
         #Setting up the architecture of the network
         #------------------------------------------
 
         #Let X_train be the first layer in the NN
-        #What shape should X be? Isn't each input meant to be a number, not a vector?
         self.input = hidden_layer(0, 0)   #doesn't need weights and biases
-
         self.input.a_out = self.X_train
 
-
         #The first layer must have different dimensions since the number of features in X is (likely) different from the number of nodes
-        self.layer1 = hidden_layer(self.n_features, self.n_hidden_nodes)
+        self.layer1 = hidden_layer(self.n_features, self.hidden_nodes[0])   #The input layer has two nodes, each containing a vector of all the datapoints
+        self.output_layer = hidden_layer(self.hidden_nodes[-1], 1)
 
-        self.output_layer = hidden_layer(self.n_hidden_nodes, 1)
-
-        #self.output_layer = hidden_layer(self.n_hidden_nodes, self.n_hidden_nodes)
 
         self.layers.append(self.input)
         self.layers.append(self.layer1)
 
 
-        for i in range (1, self.n_hidden_layers):  #Want (n_hidden_layers + 2) layers in total
-            #Make a list of the number of nodes in each hidden layer
-            #
-            i = hidden_layer(self.n_hidden_nodes, self.n_hidden_nodes)   #Assuming all layers have the same number of nodes
-            self.layers.append(i)   #list of layer-objects
+        for i in range(1, len(self.hidden_nodes)):  #Want (n_hidden_layers + 2) layers in total, start at 1, since the first layer is already made
+            layer = hidden_layer(self.hidden_nodes[i-1], self.hidden_nodes[i])   #Assuming all layers have the same number of nodes
+            self.layers.append(layer)   #list of layer-objects
 
 
         self.layers.append(self.output_layer)
 
-        #print("Output hidden bias: ", self.output_layer.hidden_bias)
 
 
 
@@ -107,8 +97,9 @@ class NeuralNetwork:
         elif self.activation_func == "RELU" or self.activation_func == "relu" or self.activation_func == "Relu":
             return relu(z)
 
-        elif self.activation_func == "softmax" or self.activation_func == "Softmax":
-            return softmax(z)
+        elif self.activation_func == "leaky_relu" or self.activation_func == "leaky_RELU":
+            return leaky_relu(z)
+
 
 
     def cost(self, a_out, z_train): #Do we need this?
@@ -130,15 +121,15 @@ class NeuralNetwork:
             else:
                 return 1
 
-        elif self.activation_func == "softmax" or self.activation_func == "Softmax":
+        elif self.activation_func == "leaky_relu" or self.activation_func == "leaky_RELU":
             pass
 
 
 
     def cost_derivative(self, z_model):
         if self.cost_func == "MSE":
-
             return (-2/self.n_datapoints)*(self.z_train.reshape(z_model.shape) - z_model)
+
 
         elif self.cost_func == "accuracy":
             pass
@@ -152,10 +143,9 @@ class NeuralNetwork:
         for layer in (self.layers[1:]):
             layer.a_in = previous.a_out
 
-            #In z_hidden, each row represents the outputs for a given layer
+            #In z_hidden, each column represents a node, each row a datapoint
             z_hidden = previous.a_out @ layer.hidden_weights + layer.hidden_bias
             a_hidden = self.activation(z_hidden)
-
 
             layer.a_out = a_hidden  #update the matrix of z_values, containing all the inputs for the next layer
 
@@ -165,51 +155,24 @@ class NeuralNetwork:
 
 
     def backpropagation(self):
-        #for layer in self.layers[1::-1]:
-        #error_output = self.output_layer.a_out - self.z_train.reshape(self.output_layer.a_out.shape)   #Wrong expression
-
         grad_cost = self.cost_derivative(self.output_layer.a_out)
 
-        #grad_cost = (-2/self.n_datapoints)*(self.z_train.reshape(self.output_layer.a_out.shape) - self.output_layer.a_out)
+        error_output = grad_cost   #don't have an activation func in the last layer, so calculate this outside
 
-        grad_activation = self.activation_derivative(self.output_layer.a_out)
-        #grad_activation = sigmoid(self.output_layer.a_out)*(1-sigmoid(self.output_layer.a_out))
-        #make this dependent on the cost function, this is the derivative of the cost function, which also includes the derivative of the cost function
-
-
-        error_output = grad_cost# * grad_activation
-
-
-
-        weights_gradient = self.output_layer.a_in.T @ error_output
-        bias_gradient = np.sum(error_output)
-
+        self.output_layer.error = error_output
 
         next_layer = self.output_layer
 
 
-        self.output_layer.update_parameters(weights_gradient, bias_gradient, self.eta)
-
-
-        error_output = grad_cost * grad_activation
-        next_error = error_output
-
         for layer in self.layers[-2:0:-1]:  #don't include the output layer or input layer
-
-
-            error = (next_error @ next_layer.hidden_weights.T) * self.activation_derivative(layer.a_out)
+            #calculate all the errors before the weights and biases are updated
+            error = (next_layer.error @ next_layer.hidden_weights.T) * self.activation_derivative(layer.a_out)
             #layer.a_out * (1 - layer.a_out)   #The last term is the derivative of the activation funciton
-
             layer.error = error
-
-            next_error = error
-
             next_layer = layer
 
 
-
-        for layer in self.layers[-2:0:-1]:  #don't include the output layer or input layer
-
+        for layer in self.layers[-1:0:-1]:  #don't include the input layer, which has no weights and biases
             weights_gradient = np.matmul(layer.a_in.T, layer.error)
             bias_gradient = np.sum(layer.error, axis=0)
 
@@ -229,7 +192,7 @@ class NeuralNetwork:
             pass
 
         if method == "test":
-            for i in range (100):
+            for i in range (10000):
 
                 self.feed_forward()
                 self.backpropagation()
@@ -242,7 +205,7 @@ class NeuralNetwork:
 
 
     def prediction(self):
-        self.X.z_out = self.X_test   #The input layer is replaced with the test data
+        self.input.a_out = self.X_test   #The input layer is replaced with the test data
         self.feed_forward()
 
         return self.output_layer.a_out  #z_prediction
@@ -254,13 +217,13 @@ class NeuralNetwork:
 
 
 class hidden_layer:   #let each layer be associated with the weights and biases that come before it
-    def __init__(self, n_features, n_hidden_nodes):
+    def __init__(self, n_previous_nodes, n_hidden_nodes):
         self.n_hidden_nodes = n_hidden_nodes
-        self.n_features = n_features
+        self.n_previous_nodes = n_previous_nodes
 
         #Initialise weights and biases
         #Initialise the weights according to a normal distribution
-        self.hidden_weights = np.random.randn(self.n_features, self.n_hidden_nodes)
+        self.hidden_weights = np.random.randn(self.n_previous_nodes, self.n_hidden_nodes)
         #The weight should be a matrix where each column is the weights for a given node
         #Only one weight matrix per layer
 
@@ -283,7 +246,7 @@ class hidden_layer:   #let each layer be associated with the weights and biases 
 
 np.random.seed(2018)
 
-n_dpoints = 5
+n_dpoints = 10
 
 noise = 0
 
@@ -301,13 +264,13 @@ z_flat = np.ravel(z)
 
 X = np.column_stack((x_flat, y_flat))
 
+
+
 X_train, X_test, z_train, z_test = train_test_split(X, z_flat, test_size=0.2)
 
 
-
-n_hidden_nodes = 3
-n_hidden_layers = 1
-eta = 0.03
+hidden_nodes = [15, 15, 10, 10, 10]   #This is a list of the number of nodes in each hidden layer
+eta = 0.04
 batch_size = 50
 epochs = 10
 lamb = 0
@@ -315,11 +278,11 @@ lamb = 0
 print('')
 print('')
 print('')
-print('')
-print('')
 
-Neural = NeuralNetwork(X_train, z_train, X_test, z_test, n_hidden_nodes, n_hidden_layers, epochs, batch_size, eta, lamb, activation_func = "sigmoid", cost_func = "MSE")
+Neural = NeuralNetwork(X_train, z_train, X_test, z_test, hidden_nodes, epochs, batch_size, eta, lamb, activation_func = "sigmoid", cost_func = "MSE")
 z_model = Neural.model_training("test")
+
+z_predict = Neural.prediction()
 
 z_model = z_model.ravel()
 
@@ -327,8 +290,10 @@ z_model = z_model.ravel()
 
 results = np.column_stack((z_train, z_model))
 
-print(results)
+#print(results)
 
-
-print("MSE: ", mean_squared_error(z_model, z_train))
-print("R2 score: ", r2_score(z_model, z_train))
+print("Train MSE: ", mean_squared_error(z_model, z_train))
+print("Train R2 score: ", r2_score(z_model, z_train))
+print('')
+print("Test MSE: ", mean_squared_error(z_predict, z_test))
+print("Test R2 score: ", r2_score(z_predict, z_test))
