6f064c7e62b500e65ed30116c24ba1d327d95b5b
SaraPJensen <sarapje@ifi.uio.no>
2021-11-10 19:51:42 +0100

    changed everything and added plotting code

--- Project2/regclass.py
+++ Project2/regclass.py
@@ -1,9 +1,23 @@
 import numpy as np
-
-from sklearn.linear_model import SGDRegressor #,LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, Normalizer# scalerStandard, scalerMinMax, scalerMean, scalerRobust
-from sklearn.metrics import mean_squared_error as MSE, r2_score as R2
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import SGDRegressor
+from numpy import linalg
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def design_matrix(x_flat, y_flat, poly):
+    l = int((poly+1)*(poly+2)/2)                #Number of elements in beta, features in X
+    X = np.ones((len(x_flat),l))
+
+    for i in range(0, poly+1):
+        q = int((i)*(i+1)/2)
+
+        for k in range(i+1):
+            X[:,q+k] = (x_flat**(i-k))*(y_flat**k)
+    return X
+
 
 def FrankeFunction(x,y):
     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
@@ -12,241 +26,231 @@ def FrankeFunction(x,y):
     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
     return term1 + term2 + term3 + term4
 
-def design_matrix(x_flat, y_flat, poly):
-    l = int((poly+1)*(poly+2)/2)                # Number of elements in beta
-    X = np.ones((len(x_flat),l))
 
-    for i in range(0, poly+1):
-        q = int((i)*(i+1)/2)
+def Franke_data(n_dpoints, noise, poly):
 
-        for k in range(i+1):
-            X[:,q+k] = (x_flat**(i-k))*(y_flat**k)
+    np.random.seed(2018)
+    x = np.arange(0,1,1/n_dpoints)
+    y = np.arange(0,1,1/n_dpoints)
 
-    return X
+    x, y = np.meshgrid(x, y)
+    z = FrankeFunction(x, y) + noise*np.random.randn(n_dpoints, n_dpoints)
 
-class Regression:
-    def __init__(self, x, y, z, maxpoly):
-        self.xf = x.ravel()
-        self.yf = y.ravel()
-        self.zf = z.ravel()
+    x_flat = np.ravel(x)
+    y_flat = np.ravel(y)
+    z_flat = np.ravel(z)
 
-        self.X = design_matrix(self.xf, self.yf, maxpoly)
-        self.X_train, self.X_test, self.z_train, self.z_test = train_test_split(self.X, self.zf, test_size = 0.2)
+    X = design_matrix(x_flat, y_flat, poly)
 
-        self.maxpoly = maxpoly
-        self.predicts = {}
-        self.models = {}
+    X_train, X_test, z_train, z_test = train_test_split(X, z_flat, test_size = 0.2)
+
+    return X_train, X_test, z_train, z_test
 
 
-    def n_features(self, poly):
-        return int( (poly + 1)*(poly + 2)/2 ) # + 1 ? "Nei" - Sara
+class FrankeRegression:
+    def __init__(self, X_train, X_test, z_train, z_test):
+        self.X_train = X_train
+        self.X_test = X_test
+        self.z_train = z_train
+        self.z_test = z_test
 
-    def scale(self, str_arg):
-        """
-        Possible args: Standard, MinMax, Mean, Robust, none
-        """
-        if str_arg.lower() == "none":
-            self.scaled = False
-            self.X_train_scaled, self.X_test_scaled, self.z_train_scaled, self.z_test_scaled =\
-                    self.X_train, self.X_test, self.z_train, self.z_test
-            return None
-        elif str_arg in ["Standard", "MinMax", "Mean", "Robust"]:
-            self.scaled = True
-            if str_arg in ["Standard", "MinMax", "Mean"]:
-                scalefunc = eval( str_arg + "Scaler")
-            if str_arg == "Normalizer":
-                scalefunc = Normalizer
-
-            self.X_train_scaled, self.X_test_scaled, self.z_train_scaled, self.z_test_scaled =\
-                    scalefunc(self.X_train, self.X_test, self.z_train, self.z_test)
-        else:
-            print("invalid argument in scale() function.\
-                    Possible args: Standard, MinMax, Mean, Normalizer")
-            quit(1)
+        self.features = len(self.X_train[0,:])
+        self.datapoints = len(self.X_train[:, 0])
 
 
-    def OLSR_beta(self, lamb):
+    def beta(self, lamb):
         I = np.eye( len( self.X_train[0, :]) )
-
-        if self.scaled == False:
-            beta = np.linalg.pinv(self.X_train.T @ self.X_train + lamb*I) @\
-                    self.X_train.T @ self.z_train
-
-        elif self.scaled == True:
-            beta = np.linalg.pinv(self.X_train_scaled.T @ self.X_train_scaled + lamb*I) @\
-                    self.X_train_scaled.T @ self.z_train_scaled
+        beta = np.linalg.pinv(self.X_train.T @ self.X_train + lamb*I) @ self.X_train.T @ self.z_train
 
         return beta
 
-    def OLS_Ridge(self, scale_arg = "none", scale = True, lamb = 0):
-        self.scale(scale_arg)
 
-        beta = self.OLSR_beta(lamb)
-        #z_model = self.X_train_scaled @ beta
-        #z_predict = self.X_test_scaled @ beta
+    def OLS_Ridge(self, lamb):
+        beta = self.beta(lamb)
 
-        self.OLSRmodel = self.X_train_scaled @ beta
-        self.OLSRpred = self.X_test_scaled @ beta
+        z_model = self.X_train @ beta
+        z_predict = self.X_test @ beta
 
-        if lamb == 0:
-            self.predicts['OLS'] = self.OLSRpred
-            self.models['OLS'] = self.OLSRmodel
-        elif lamb > 0:
-            self.predicts['Ridge'] = self.OLSRpred
-            self.models['Ridge'] = self.OLSRmodel
+        return z_model, z_predict
 
-    def eta(self):
-        #Add some optionality for changing the learning rate
-        pass
 
-    def GD(self, n_iterations = 100000, eta = 0.0025, lamb = 0): # Gradient descent
-        features = self.n_features(self.maxpoly)
-        theta = np.random.randn(features, 1)
-        #eta = 0.0025
-        eig_vals, eig_vecs = np.linalg.eig(self.X_train.T @ self.X_train)
-        eta_GD = (1 / np.max(eig_vals))
+    def max_eta(self):
+        matrix = self.X_train.T @ self.X_train
+        eig_vals, eig_vecs = np.linalg.eig(matrix)
+        eta = (1 / np.max(eig_vals))  #calculate the maximum eta
 
-        #n_iterations = 100000
+        return eta
+
+
+    def GD(self, epochs, eta = 0, momentum = 0, lamb = 0): # Gradient descent
+
+        theta = np.random.randn(self.features, 1)
+        if eta == 0:
+            eta = self.max_eta()
 
         z_train = self.z_train.reshape(-1, 1)
+        dtheta = 0
 
-        #Define the cost function to be able to take the gradient using autograd
-        def cost_func(X_train, theta, z_train):
-            return (1/len(X_train[:, 0]))*((X_train @ theta)-z_train)**2      #np.sum(((X_train @ theta)-z_train)**2)
-
-
-
-        for iter in range(n_iterations):
-
-            grad = self.X_train.T @ ((self.X_train @ theta)-z_train) + lamb * theta   #replace this with autograd
-            #Should this be n_dpoints???   Removed at the start:  2.0/ n_dpoints *
-            #derivative = egrad(cost_func, 1)
-            #gradients = derivative(X_train, theta, z_train)   #take the derivative w.r.t. theta
-
-            if np.linalg.norm(grad) <= 0.00001:
+        for iter in range(epochs):
+            gradient = self.X_train.T @ ((self.X_train @ theta) - z_train) + lamb * theta
+            if abs(sum(gradient)) <= 0.00001:
                 break
 
-            theta -= eta_GD * grad
+            dtheta = momentum * dtheta - eta*gradient
+            theta += dtheta
 
-        #print("GD theta: ", theta)
+        z_model = self.X_train @ theta
+        z_predict = self.X_test @ theta
 
-        #z_predict = self.X_test @ theta
-        #z_model = self.X_train @ theta
-
-        #error_estimate(self, self.z_train, self.z_test, z_model, z_predict)
-        self.GDpred = self.X_test @ theta
-        self.GDmodel = self.X_train @ theta
-
-        #self.predicts.append(self.GDpred)
-        #self.models.append(self.GDmodel)
-        self.predicts['GD'] = self.GDpred
-        self.models['GD'] = self.GDmodel
+        return z_model, z_predict
 
 
-    def SGD(self, epochs = 1000, batch_size = 5, eta = 0.0025, lamb = 0): # Stochastic Gradient descent
-        #epochs = 1000
-        features = self.n_features(self.maxpoly)
-        #batch_size = 5
+
+    def SGD(self, epochs, batch_size, eta = 0, momentum = 0, lamb = 0): # Stochastic Gradient descent
         batches = int(len(self.X_train[:, 0])/batch_size)
-        #eta = 0.0025   #need to add some algorithm to scale the learning rate
-        theta = np.random.randn(features, 1)
 
+        if eta == 0:
+            eta = self.max_eta()
+
+        theta = np.random.randn(self.features, 1)
         z_train = self.z_train.reshape(-1, 1)
+        dtheta = 0
+
 
         for e in range (epochs):
-            indices = np.random.permutation(len(self.X_train[:, 0]))
+            indices = np.random.permutation(self.datapoints)
             indices = np.array_split(indices, batches)
-            for b in range (batches):
-                #indices = np.random.randint(0, high = len(self.X_train[:, 0]), size = batch_size)
-                X_b = self.X_train[indices[b]]
-                z_b = z_train[indices[b]]
-                grad = X_b.T @ ((X_b @ theta)-z_b) + lamb * theta
 
-                if np.linalg.norm(grad) <= 0.00001:
+            for b in range (batches):
+                index = np.random.randint(batches)
+
+                X_b = self.X_train[indices[index],:]   #pick out what rows to use
+                z_b = z_train[indices[index]]
+
+                gradient = X_b.T @ ((X_b @ theta)-z_b) + lamb * theta
+
+                if abs(sum(gradient)) <= 0.00001:
                     break
 
-                theta -= eta*grad
+                dtheta = momentum * dtheta - eta*gradient
+                theta += dtheta
 
 
-        #print("SGD theta: ", theta)
+        z_predict = self.X_test @ theta
+        z_model = self.X_train @ theta
 
-        self.SGDpred = self.X_test @ theta
-        self.SGDmodel = self.X_train @ theta
-    
-        if lamb == 0:
-            self.predicts['SGD'] = self.SGDpred
-            self.models['SGD'] = self.SGDmodel
-        elif lamb > 0:
-            self.predicts['SGD_lamb'] = self.SGDpred
-            self.models['SGD_lamb'] = self.SGDmodel
-
-    def sk_SGD(self, lamb = 0):
-        SGDReg = SGDRegressor(max_iter = 10000, alpha=lamb,\
-                learning_rate = 'constant')
-        SGDReg.fit(self.X_train, self.z_train)
-
-        self.sk_SGDpred = SGDReg.predict(self.X_test)
-        self.sk_SGDmodel = SGDReg.predict(self.X_train)
-
-        if lamb == 0:
-            self.predicts['sk_SGD'] = self.sk_SGDpred
-            self.models['sk_SGD'] = self.sk_SGDmodel
-        elif lamb > 0:
-            self.predicts['sk_SGD_lamb'] = self.sk_SGDpred
-            self.models['sk_SGD_lamb'] = self.sk_SGDmodel
+        return z_model, z_predict
 
 
-    def get_error(self):
-        for i in self.predicts:
-            print(i)
-            print(f"MSE, pred: {MSE(self.predicts[i], self.z_test)}")
-            print(f"MSE, model: {MSE(self.models[i], self.z_train)}")
-            print(f"R2, pred: {R2(self.predicts[i], self.z_test)}")
-            print(f"R2, model: {R2(self.models[i], self.z_train)}\n\n")
-            
+    def error(self, z_model, z_predict):
+        mse_train = mean_squared_error(self.z_train, z_model)
+        mse_test = mean_squared_error(self.z_test, z_predict)
+        r2_train = r2_score(self.z_train, z_model)
+        r2_test = r2_score(self.z_test, z_predict)
+
+        return mse_train, mse_test, r2_train, r2_test
 
 
-        #print(MSE(self.OLSRpred, self.z_test))
-        #print(MSE(self.GDpred, self.z_test))
 
-    '''
-    def error_estimate(self, z_train, z_test, z_model, z_predict):
-        mse_train = mean_squared_error(z_train, z_model)
+
+    def print_error(self, z_model, z_predict):
+        mse_train = mean_squared_error(self.z_train, z_model)
         print(f"MSE, train: {mse_train:.5}")
         print('')
-        mse_test = mean_squared_error(z_test, z_predict)
+        mse_test = mean_squared_error(self.z_test, z_predict)
         print(f"MSE, test: {mse_test:.5}")
         print('')
 
-        r2_train = r2_score(z_train, z_model)
+        r2_train = r2_score(self.z_train, z_model)
         print(f"R2, train: {r2_train:.5}")
         print('')
-        r2_test = r2_score(z_test, z_predict)
+        r2_test = r2_score(self.z_test, z_predict)
         print(f"R2, test: {r2_test:.5}")
         print('')
-    '''
 
-if __name__ == "__main__":
-    n = 20
-    x = np.random.rand(n)
-    y = np.random.rand(n)
 
-    x, y = np.meshgrid(x, y)
 
-    noise = 0.15
-    z = FrankeFunction(x, y) + noise * np.random.randn(n, n)
 
-    maxpoly = 10
-    reg = Regression(x, y, z, maxpoly)
 
-    reg.OLS_Ridge()
-    reg.OLS_Ridge(lamb = 0.001)
-    reg.GD()
-    epochs = 1000
-    batch_size = 5
-    reg.SGD(epochs = epochs, batch_size = batch_size, lamb = 0.01)
 
-    #print(f"epochs = {epochs}, batch size = {batch_size}")
-    reg.SGD(epochs = epochs, batch_size = batch_size)
-    reg.sk_SGD()
-    reg.get_error()
+X_train, X_test, z_train, z_test = Franke_data(n_dpoints = 30, noise = 0.05, poly=8)
+
+
+'''
+GD = FrankeRegression(X_train, X_test, z_train, z_test)
+z_model, z_predict = GD.GD(1000, eta = 0, momentum = 0, lamb = 0)
+print("Gradient descent error:")
+GD.print_error(z_model, z_predict)
+
+print()
+
+SGD = FrankeRegression(X_train, X_test, z_train, z_test)
+z_model, z_predict = SGD.SGD(1000, eta = 0.0025, batch_size = 30, momentum = 0, lamb = 0)
+print("Stochastic gradient descent error:")
+SGD.print_error(z_model, z_predict)
+
+print()
+
+OLS = FrankeRegression(X_train, X_test, z_train, z_test)
+z_model, z_predict = OLS.OLS_Ridge(lamb = 0)
+print("OLS error:")
+OLS.print_error(z_model, z_predict)
+
+print()
+
+Ridge = FrankeRegression(X_train, X_test, z_train, z_test)
+z_model, z_predict = Ridge.OLS_Ridge(lamb = 0.05)
+print("Ridge error:")
+Ridge.print_error(z_model, z_predict)
+'''
+
+
+
+
+
+#gridsearch for lambda and eta, here for SGD
+eta_min = -7   #log base 10
+eta_max = -2
+eta_n = 2
+eta = np.logspace(eta_min, eta_max, eta_n)
+
+lamb_min = -7   #log base 10
+lamb_max = -2
+lamb_n = 2
+
+
+lamb = np.logspace(lamb_min, lamb_max, lamb_n)
+
+mse_results = np.zeros((len(lamb), len(eta)))   #each row corresponds to one value of lambda, each column to a value of eta
+r2_results = np.zeros((len(lamb), len(eta)))
+
+
+for e in range(len(eta)):
+    for l in range(len(lamb)):
+        np.random.seed(123)
+        SGD = FrankeRegression(X_train, X_test, z_train, z_test)
+        z_model, z_predict = SGD.SGD(1000, eta = eta[e], batch_size = 30, momentum = 0, lamb = lamb[l])
+        mse_train, mse_test, r2_train, r2_test = SGD.error(z_model, z_predict)
+        mse_results[l, e] = mse_test  #row l, column e
+        r2_results[l, e] = r2_test
+
+        print(e, l)
+
+
+min = np.min(mse_results)
+index = np.where(mse_results == min)
+print("Min MSE: ", min)
+print("Min eta: ", eta[index[1]])
+print("Min lambda: ", lamb[index[0]])
+
+
+eta = np.round(np.log10(eta), 3)
+lamb = np.round(np.log10(lamb), 3)
+
+ax_mse = sns.heatmap(mse_results, xticklabels = eta, yticklabels = lamb,  annot=True, cmap="YlGnBu", fmt='.4g')
+
+ax_mse.set_title("MSE")
+ax_mse.set_xlabel(r"log10$\eta$")
+ax_mse.set_ylabel(r"log10$\lambda$")
+
+plt.show()
