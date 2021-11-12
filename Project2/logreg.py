1a5a13a61f57c6a8231667474027b1f8eb6d0b03
daniejaa <daniejaa>
2021-11-12 14:46:54 +0100

    'Complete' code for part e, generates results to part_e folder

--- Project2/logreg.py
+++ Project2/logreg.py
@@ -33,137 +33,159 @@ X_test_s = scaler.fit_transform(X_test)
 def p(z):       # Sigmoid / Logistic function
     return 1/(1+np.exp(-z))
 
-#if train:
 """ Grid search for optimal params """
-learning_rates = np.logspace(-4, 1, 2)  # 11
-lambdas = np.logspace(-5, 1, 2)         # 20
+learning_rates = np.logspace(-4, 1, 11)  # 11
+lambdas = np.logspace(-5, 1, 20)         # 20
 
 n_features = len(X_train[0])
 y_train = y_train.reshape(-1, 1)#np.array([y_train]).reshape(-1, 1)
 y_test = y_test.reshape(-1, 1)
 max_iterations = 10000
 
-sk_logreg = np.zeros(len(lambdas))
-gd_score = np.zeros((len(learning_rates), len(lambdas)))
-sk_sgd_score = np.zeros((len(learning_rates), len(lambdas)))
-man_sgd_score = np.zeros((len(learning_rates), len(lambdas)))
+sklr_filename = f"part_e/sk_loglreg_{len(lambdas)}"
+gd_filename = f"part_e/man_gd_{len(learning_rates)}_{len(lambdas)}"
+sksgd_filename = f"part_e/sk_sgd_{len(learning_rates)}_{len(lambdas)}"
+mansgd_filename = f"part_e/man_sgd_{len(learning_rates)}_{len(lambdas)}"
 
-for l, lambd in enumerate(lambdas):
-    """ Sk LogReg """ # only depends on lambda
-    ''' By default the regression is used with the lBFGS
-        (limited-memory Broyden-Fletcher-Goldfarb-Shanno) method,
-        a quasi-Newton method. Newton-Cotes however is an option.
+
+if train:
+
+    sk_logreg = np.zeros(len(lambdas))
+    gd_score = np.zeros((len(learning_rates), len(lambdas)))
+    sk_sgd_score = np.zeros((len(learning_rates), len(lambdas)))
+    man_sgd_score = np.zeros((len(learning_rates), len(lambdas)))
+
+    for l, lambd in enumerate(lambdas):
+        #print(f"Iterasjon:\nlambda {l+1}/{len(lambdas)}")
+        """ Sk LogReg """ # only depends on lambda
+        ''' By default the regression is used with the lBFGS
+            (limited-memory Broyden-Fletcher-Goldfarb-Shanno) method,
+            a quasi-Newton method. Newton-Cotes however is an option.
+        '''
+        LogReg = LogisticRegression(max_iter = max_iterations, penalty='l2', C = 1/lambd,\
+                multi_class = 'multinomial')
+        LogReg.fit(X_train, y_train.ravel())
+
+        sk_logreg[l] = LogReg.score(X_test, y_test)
+        
+        for e, eta in enumerate(learning_rates):
+            print(f"Iteration: lambda {l+1}/{len(lambdas)} eta {e+1}/{len(learning_rates)}")
+            beta = 0.01 * np.random.randn(n_features, 1)    # Initial random parameter inizialization
+    #beta = LogReg.coef_.reshape(-1, 1)
+
+    #ee = np.exp(z)
+    #return ee/(1+ee)
+
+    #print(z)
+    #print(p(z))
+    #print(p(z)*(1-p(z)))
+    #print(W)
+
+    #y_train_s= scaler.fit_transform(y_train.reshape(-1, 1))
+    #y_test_s = scaler.fit_transform(y_test.reshape(-1, 1))
+
+
+            """ Gradient Descent """
+            for i in range(max_iterations):
+                #print("iteration: ", i)
+                z = X_train_s.dot(beta)
+                #print("z: ", np.shape(z))
+                W = np.diag( (p(z)*(1-p(z))).reshape(-1) )
+                #print("W: ", W.shape)
+
+                #print("y train: ", y_train.shape)
+                #print("p(z): ", p(z).shape)
+                grad_C = 1/len(X_train) * X_train_s.T @ (y_train - p(z)) + lambd * beta
+                #1/len(X_train) *
+                #print("Grad C: ", grad_C.shape)
+                Hess_C = X_train_s.T @ W @ X_train_s
+                #print("Hess C: ", Hess_C.shape)
+
+                #beta = beta + np.linalg.pinv(Hess_C) @ grad_C       # Bør det være + her? ref. Hastie
+                beta = beta + eta * grad_C      # Const learn_rate, no overflow
+                #print("Beta: ", beta.shape)
+
+            #print("Newton iteration")
+            #print(LogReg.coef_)
+            #print(beta.reshape(1, -1))
+            #pred = out(p(X_test_s.dot(beta)))
+            prob = p(X_test_s.dot(beta))
+            #print(prob)
+            pred = prob.round()
+
+            score = np.sum(pred == y_test)/len(y_test)
+            gd_score[e, l] = score
+
+            """ Stochastic Gradient Descent """
+            #sklearn
+
+            SGDLogReg = SGDClassifier(loss = 'log', max_iter = max_iterations, alpha=lambd,\
+                    learning_rate='constant', eta0 = eta)
+            SGDLogReg.fit(X_train, y_train.ravel())
+            SGDaccuracy = SGDLogReg.score(X_test, y_test)
+
+            sk_sgd_score[e, l] = SGDaccuracy
+
+
+            epochs = 10000
+            batch_size = 1
+            num_batches = int(n_features/batch_size) 
+            theta = np.random.randn(n_features, 1)
+
+            tol = 1e-6
+            for ep in range(epochs):
+                for b in range(num_batches):
+                    indeces = np.random.randint(0, high = n_features, size = batch_size)
+
+                    X_b = X_train_s[indeces]
+                    y_b = y_train[indeces]
+
+
+                    z = X_b.dot(theta)
+                    #grad = (1/batch_size) * X_b.T @ ((X_b @ theta) - y_b)
+                    grad = -(1/batch_size) * X_b.T @ (y_b - p(z)) + lambd * theta# Funker med - (negativ)?
+                    #print(grad.shape)
+                    
+                    if np.linalg.norm(grad) < tol:
+                        break
+
+                    #print(theta.reshape(1, -1))
+                    theta = theta - eta*grad
+
+            #print("SGD")
+            #print(SGDLogReg.coef_)
+            #print(theta.reshape(1, -1))
+
+            test_prob = p(X_test_s.dot(theta))
+            #print(test_prob.reshape(1, -1))
+            #print(y_test)
+            #print(p(X_test_s.dot(SGDLogReg.coef_.reshape(-1, 1))).reshape(1, -1))
+            #test_prob = p(X_test_s.dot(LogReg.coef_.reshape(-1, 1)))
+            #print(test_prob) # Gir zeros
+            man_pred = test_prob.round()
+            man_sgd_score[e, l] = np.sum(man_pred==y_test)/len(y_test)
+
+            #man_sgd_score = test_prob.round()
+
     '''
-    LogReg = LogisticRegression(max_iter = max_iterations, penalty='l2', C = 1/lambd,\
-            multi_class = 'multinomial')
-    LogReg.fit(X_train, y_train.ravel())
+    print(f"Sk-learn Logistic Regression:\n{sk_logreg}")
+    print(f"Gradient descent:\n{gd_score}")
+    print(f"Sk-learn SGD:\n{sk_sgd_score}")
+    print(f"Manual SGD:\n{man_sgd_score}")
+    '''
 
-    sk_logreg[l] = LogReg.score(X_test, y_test)
-    
-    for e, eta in enumerate(learning_rates):
+    np.savetxt(sklr_filename + ".txt", sk_logreg)
+    np.savetxt(gd_filename + ".txt", gd_score)
+    np.savetxt(sksgd_filename + ".txt", sk_sgd_score)
+    np.savetxt(mansgd_filename + ".txt", man_sgd_score)
 
-        beta = 0.01 * np.random.randn(n_features, 1)    # Initial random parameter inizialization
-#beta = LogReg.coef_.reshape(-1, 1)
+else:
+    sk_logreg = np.loadtxt(sklr_filename + ".txt")
+    gd_score = np.loadtxt(gd_filename + ".txt")
+    sk_sgd_score = np.loadtxt(sksgd_filename + ".txt")
+    man_sgd_score = np.loadtxt(mansgd_filename + ".txt")
 
-#ee = np.exp(z)
-#return ee/(1+ee)
-
-#print(z)
-#print(p(z))
-#print(p(z)*(1-p(z)))
-#print(W)
-
-#y_train_s= scaler.fit_transform(y_train.reshape(-1, 1))
-#y_test_s = scaler.fit_transform(y_test.reshape(-1, 1))
-
-
-        """ Gradient Descent """
-        for i in range(max_iterations):
-            #print("iteration: ", i)
-            z = X_train_s.dot(beta)
-            #print("z: ", np.shape(z))
-            W = np.diag( (p(z)*(1-p(z))).reshape(-1) )
-            #print("W: ", W.shape)
-
-            #print("y train: ", y_train.shape)
-            #print("p(z): ", p(z).shape)
-            grad_C = 1/len(X_train) * X_train_s.T @ (y_train - p(z)) + lambd * beta
-            #1/len(X_train) *
-            #print("Grad C: ", grad_C.shape)
-            Hess_C = X_train_s.T @ W @ X_train_s
-            #print("Hess C: ", Hess_C.shape)
-
-            #beta = beta + np.linalg.pinv(Hess_C) @ grad_C       # Bør det være + her? ref. Hastie
-            beta = beta + eta * grad_C      # Const learn_rate, no overflow
-            #print("Beta: ", beta.shape)
-
-        #print("Newton iteration")
-        #print(LogReg.coef_)
-        #print(beta.reshape(1, -1))
-        #pred = out(p(X_test_s.dot(beta)))
-        prob = p(X_test_s.dot(beta))
-        #print(prob)
-        pred = prob.round()
-
-        score = np.sum(pred == y_test)/len(y_test)
-        gd_score[e, l] = score
-
-        """ Stochastic Gradient Descent """
-        #sklearn
-
-        SGDLogReg = SGDClassifier(loss = 'log', max_iter = max_iterations, alpha=lambd,\
-                learning_rate='constant', eta0 = eta)
-        SGDLogReg.fit(X_train, y_train.ravel())
-        SGDaccuracy = SGDLogReg.score(X_test, y_test)
-
-        sk_sgd_score[e, l] = SGDaccuracy
-
-
-        epochs = 10000
-        batch_size = 1
-        num_batches = int(n_features/batch_size) 
-        theta = np.random.randn(n_features, 1)
-
-        tol = 1e-6
-        for ep in range(epochs):
-            for b in range(num_batches):
-                indeces = np.random.randint(0, high = n_features, size = batch_size)
-
-                X_b = X_train_s[indeces]
-                y_b = y_train[indeces]
-
-
-                z = X_b.dot(theta)
-                #grad = (1/batch_size) * X_b.T @ ((X_b @ theta) - y_b)
-                grad = -(1/batch_size) * X_b.T @ (y_b - p(z)) + lambd * theta# Funker med - (negativ)?
-                #print(grad.shape)
-                
-                if np.linalg.norm(grad) < tol:
-                    break
-
-                #print(theta.reshape(1, -1))
-                theta = theta - eta*grad
-
-        #print("SGD")
-        #print(SGDLogReg.coef_)
-        #print(theta.reshape(1, -1))
-
-        test_prob = p(X_test_s.dot(theta))
-        #print(test_prob.reshape(1, -1))
-        #print(y_test)
-        #print(p(X_test_s.dot(SGDLogReg.coef_.reshape(-1, 1))).reshape(1, -1))
-        #test_prob = p(X_test_s.dot(LogReg.coef_.reshape(-1, 1)))
-        #print(test_prob) # Gir zeros
-        man_pred = test_prob.round()
-        man_sgd_score[e, l] = np.sum(man_pred==y_test)/len(y_test)
-
-        #man_sgd_score = test_prob.round()
-
-print(f"Sk-learn Logistic Regression:\n{sk_logreg}")
-print(f"Gradient descent:\n{gd_score}")
-print(f"Sk-learn SGD:\n{sk_sgd_score}")
-print(f"Manual SGD:\n{man_sgd_score}")
-
+print(gd_score)
 '''
 sk_ans = SGDLogReg.predict(X_test)#out(p(X_test_s.dot(SGDLogReg.coef_.reshape(-1, 1))))
 
