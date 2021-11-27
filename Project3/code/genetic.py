437050bc848603e06215cd1d3bd1e00f5586578b
SaraPJensen <sarapje@ifi.uio.no>
2021-11-27 16:45:40 +0100

    possibly working

--- Project3/code/genetic.py
+++ Project3/code/genetic.py
@@ -3,6 +3,7 @@ from autograd import grad
 import random
 import autograd.numpy as np
 from scipy.stats import halfnorm
+import math
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -14,34 +15,24 @@ class Chromosome:
         self.g = 0
         self.equation = self.expression(self.genes[0])
 
+        if ("x" not in self.equation) or ("t" not in self.equation):
+            self.equation += "+" + self.expression(self.genes[self.g])
+
         if "stop" in self.equation:
             self.equation = "0.0"
-
-        #print("Equation: ", self.equation)
-        #print("")
 
 
     def read_genes(self):
         self.g += 1
-        #print("G: ", self.g)
+
         if self.g > (len(self.genes) - 1):
-            #print("This should stop now...")
             return "stop"
 
         return self.genes[self.g]
 
 
-    def read_equation(self):
-        print(self.equation)
-
-    def return_genes(self):
-        return self.genes
-
-
     def expression(self, index, stop = False):
-        #print("Access expression")
         if index == "stop":
-            #print("Expression stop")
             return "stop"
 
         i = index % 6
@@ -52,9 +43,7 @@ class Chromosome:
 
 
     def operator(self, index, stop = False):
-        #print("Access operator")
         if index == "stop":
-            #print("Operator stop")
             return "stop"
 
         i = index % 4
@@ -62,19 +51,15 @@ class Chromosome:
         return ope[i]
 
     def func(self, index, stop = False):
-        #print("Access func")
         if index == "stop":
-            #print("Func stop")
             return "stop"
 
-        i = index % 6
-        func = {0: "np.sin", 1: "np.cos", 2: "np.exp", 3: "np.log", 4: "x**", 5: "t**"}
+        i = index % 5
+        func = {0: "np.sin", 1: "np.cos", 2: "np.exp", 3: "x**", 4: "t**"}
         return func[i] + "(" + self.expression(self.read_genes()) + ")"
 
     def digit(self, index, stop = False):
-        #print("Access digit")
         if index == "stop":
-            #print("Digit stop")
             return "stop"
 
         num = str(index % 10)+".0"
@@ -82,25 +67,34 @@ class Chromosome:
         return num
 
 
-    def der(self, x, t):
-        #print("Equation: ", self.equation)
+    def der_diff(self, x, t):   #calculates the derivatives for the diffusion equation
         func = lambda x, t: eval(self.equation)
-
-        #print("Function calculated")
         dM_dx = grad(grad(func, 0), 0)
-        #print("dM_dx calculated")
-
-        #print("Derivative: ", dM_dx(x, t))
-
-
-
         dM_dt = grad(func, 1)
-        #print("dM_dt calculated")
 
-        return dM_dx(x, t), dM_dt(x, t)
+        diff = (dM_dx(x, t) - dM_dt(x, t))**2
+
+        return diff
+
+    def boundary(self, x_range, t_range):
+        func = lambda x, t: eval(self.equation)
+        x_0 = 0
+        x_L = 0
+        t_0 = 0
+
+        for t in t_range:
+            x_0 += (func(0.000000001, t))**2
+            x_L += (func(x_range[-1], t))**2
+
+        for x in x_range:
+            t_0 += (func(x, 0.000000001) - np.sin(np.pi*x))**2
+
+        return x_0 + x_L + t_0
+
 
 
     def calc_fitness(self, x_range, t_range):
+
         if ("x" not in self.equation) or ("t" not in self.equation):
             self.fitness = np.inf
 
@@ -108,45 +102,32 @@ class Chromosome:
             self.fitness = 0
             for x in x_range:
                 for t in t_range:
-                    dx, dt = self.der(x, t)
-                    E = (dx - dt)**2
-                    self.fitness += E
+                    diff = self.der_diff(x, t)
+                    self.fitness += diff
 
-        #return self.fitness
+
+
+        boundary = self.boundary(x_range, t_range)
+        self.fitness += boundary
+
 
     def __gt__(self, other):
         return self.fitness > other.fitness
-        #print("Fitness calculated")
-
 
 
     def get_fitness(self):
         return self.fitness
 
 
-
-print()
-print()
-
-x_range = np.linspace(1, 20, 5)
-t_range = np.linspace(1, 20, 5)
+    def get_equation(self):
+        return self.equation
 
 
+    def return_genes(self):
+        return self.genes
 
 
-#genes = [0, 0, 4, 2, 5, 0, 5]   #x*x + t
-'''
-genes = [3, 3]
 
-C = Chromosome(genes)
-
-C.read_equation()
-
-C.fitness(x_range, t_range)
-
-print(C.get_fitness())
-
-'''
 
 np.random.seed(123)
 
@@ -154,13 +135,11 @@ class Population:
     def __init__(self, size_pop, size_chrom, generations, x_range, t_range):
         self.size_pop = size_pop    #no. of chromosomes
         self.size_chrom = size_chrom    #no. of genes in each chromosome
-
         self.generations = generations    #no. of generations
-
         self.Chromosomes = []
-
         self.x_range = x_range
         self.t_range = t_range
+
 
         for c in range (0, size_pop):
             genes = random.sample(range(0, 255), size_chrom)
@@ -172,57 +151,135 @@ class Population:
     def fitness(self):
         i = 0
         fitness_vals = np.zeros(self.size_pop, dtype=Chromosome)    #does this do anything now??
+
         for c in self.Chromosomes:
             c.calc_fitness(self.x_range, self.t_range)
-        self.Chromosomes = sorted(self.Chromosomes)
-            #print("Chromosome: ", i)
+            #print("In the list: ", c.get_fitness())
             #c.read_equation()
-            #fitness_vals.append(c.fitness(self.x_range, self.t_range))
-            #print("Fitness value: ", c.get_fitness())
             #print()
-            #i += 1
 
-        print("Chromosome fitness vals:")
+        print()
         for c in self.Chromosomes:
-            print(c.get_fitness())
+            if (math.isnan(c.get_fitness()) or math.isinf(c.get_fitness())) or not np.isfinite(c.get_fitness()):
+                #print("To be removed: ", c.get_fitness())
+                self.Chromosomes.remove(c)
+
+
+        self.remaining = len(self.Chromosomes)
+        self.Chromosomes = sorted(self.Chromosomes)
+
+
+        print()
+        print("Final chromosome fitness vals:")
+        for c in self.Chromosomes:
             #c.read_equation()
-            #print()
+            print("Fitness value: ", c.get_fitness())
 
 
-    def breed(self):
-        chroms = halfnorm.rvs(loc = 0, scale = 0.2*self.size_chrom, size = 2*self.size_pop).astype(int)
+
+
+
+    def breed_mix(self):   #this gives poor results
+        parents = 2*self.size_pop
+
+        #Find the chromosomes to reproduce to the next generation by using half a normal distribution with
+        #standard deviation 0.2*current size of population to ensure that the best individuals are reproduced
+        chroms = halfnorm.rvs(loc = 0, scale = 0.2*self.remaining, size = parents).astype(int)
 
         self.past_gen = self.Chromosomes
+        self.Chromosomes = []
 
         i = 0
-
-        while i < self.size_pop * 2:
+        j = 0
+        while i < parents:
 
             indices = np.random.randint(0, 49, 25)   #find which genes to swap
-
             new_genes = self.past_gen[chroms[i]].return_genes()
 
             for index in indices:
                 new_genes[index] = self.past_gen[chroms[i+1]].return_genes()[index]
 
-            if i % 20 == 0:   #do this for 10% of the chromosomes
+            if i % int(parents*0.1):   #do this for 10% of the chromosomes
                 new_genes = self.mutate(new_genes)
 
-            self.Chromosomes[i] = Chromosome(new_genes)
+            self.Chromosomes.append(Chromosome(new_genes))
 
             i += 2
+            j += 1
 
-    def mutate(genes):
+
+
+    def breed_swap(self):
+        parents = 2*self.size_pop
+        chroms = halfnorm.rvs(loc = 0, scale = 0.2*self.remaining, size = parents).astype(int)
+        self.past_gen = self.Chromosomes
+        self.Chromosomes = []
+
+        i = 0
+        j = 0
+        while i < parents:
+
+            index = np.random.randint(0, 30)   #find where to swap
+            new_genes = []
+
+            new_genes[:index] = self.past_gen[chroms[i]].return_genes()[:index]  #use the first half of the genes from one chromosome, the second half of the other
+            new_genes[index:] = self.past_gen[chroms[i+1]].return_genes()[index:]
+
+            if i % int(parents*0.1) == 0:   #do this for 20% of the chromosomes
+                new_genes = self.mutate(new_genes)
+
+            new_genes = self.mutate(new_genes)
+            self.Chromosomes.append(Chromosome(new_genes))
+
+            i += 2
+            j += 1
+
+
+        print("Final length: ", len(self.Chromosomes))
+
+
+    def mutate(self, genes):
         #Makes a random mutation to one of the genes by replacing it with a random number
-        index = np.random.randint(0, 49, 1)
-        genes[index] = np.random.randint(0, 255, 1)
+        index = np.random.randint(0, 49)   #find where to swap
+        genes[index] = index = np.random.randint(0, 255)   #find where to swap
         return genes
+
+
+    def print_eqs(self):
+        for c in self.Chromosomes:
+            print(c.get_equation())
+            print(c.get_fitness())
+
+            print()
+
+
+
+
+x_range = np.linspace(0.000001, 1, 10)
+t_range = np.linspace(0.000001, 1, 10)
 
 
 
 
 Pop = Population(20, 50, 1, x_range, t_range)
 
-Pop.fitness()
 
-print("Is this the right document?")
+generations = 10
+
+for i in range(generations):
+    print()
+    print("Generation: ", i)
+    print()
+    Pop.fitness()
+    Pop.breed_swap()
+
+
+Pop.fitness()
+Pop.print_eqs()
+
+
+print()
+
+#np.exp((np.sin(t)))/8.0
+
+#t*(x*t-x)/8.0     #This came up as a solution with very low fitness, 0.22
