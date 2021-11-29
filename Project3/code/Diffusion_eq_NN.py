9bcb004c38c54753e385047bb82df2e0e5ef4728
haakooto <hakonolav.torvik@gmail.com>
2021-11-29 12:12:30 +0100

    more plots

--- Project3/code/Diffusion_eq_NN.py
+++ Project3/code/Diffusion_eq_NN.py
@@ -18,24 +18,33 @@ class Diffusion(PDE_solver_NN_base):
         return np.sin(np.pi * x[:, None]) * (1 + t[:, None] * self(X, P))
 
     def eta(self, epoch):
-        return self.eta0 # * (1.3 - epoch / self.epochs)
+        return self.eta0 * (1 - 0 * epoch / self.epochs)
 
 
 def main():
     x = np.linspace(0, 1, 11)
-    t = np.linspace(0, 1.5, 101)
+    t = np.linspace(0, 3, 101)
     x, t = np.meshgrid(x, t)
     X = np.concatenate((x.reshape(-1, 1), t.reshape(-1, 1)), axis=1)
-    nodes = [20, 20]
-    epochs = 50000
+    nodes = [40, 20, 20, 20, 10]
+    epochs = 1000
     eta0 = 0.0001
 
-    Solver = Diffusion(X, epochs, nodes, eta0, load=False, name="Alice")
-    solution = Solver.get_solution(*X.T)
+    Solver = Diffusion(X, epochs, nodes, eta0, load=False, name="Beatrice", seed=2021)
+    solution = Solver.get_solution()  # trial function solution
     solution = solution.reshape(x.shape)
+    raw_solution = Solver(X, Solver.P)  # the way the net looks like without trial function
+    raw_solution = raw_solution.reshape(x.shape)
+
+    fig = go.Figure(data=[go.Scatter(x=Solver.t[10:], y=Solver.history[10:], mode="lines")])
+    fig.show()
 
     fig = go.Figure(data=[go.Surface(x=x, y=t, z=solution)])
     fig.show()
 
+    fig = go.Figure(data=[go.Surface(x=x, y=t, z=raw_solution)])
+    fig.show()
+
 if __name__ == "__main__":
     main()
+
