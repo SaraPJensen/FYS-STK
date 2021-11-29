d82921b40e0dc1110c5c4e5108f85da9641d002a
haakooto <hakonolav.torvik@gmail.com>
2021-11-29 12:12:12 +0100

    add docstrings, save history, some other stuff

--- Project3/code/PDE_solver_NN.py
+++ Project3/code/PDE_solver_NN.py
@@ -16,10 +16,10 @@ class Activations:
 
 
 class PDE_solver_NN_base(Activations):
-    def __init__(self, X, epochs, nodes, eta0, activation="relu", load=False, name=None):
+    def __init__(self, X, epochs, nodes, eta0, activation="relu", load=False, name=None, seed=21345):
         self.alpha = 0.01  # for PReLU (leaky relu)
         self.nodes = np.array([X.shape[1], *nodes, 1])
-        self.rng = np.random.default_rng(21345)
+        self.rng = np.random.default_rng(seed)
 
         self.epochs = epochs
         self.eta0 = eta0
@@ -30,6 +30,7 @@ class PDE_solver_NN_base(Activations):
                        }
         self.act_func = activations[activation]
         self.X = X
+        self.t = np.arange(self.epochs)
 
         if not load:
             P = self.initialize_weights(activation)
@@ -37,11 +38,12 @@ class PDE_solver_NN_base(Activations):
             print(f"Initial cost: {self.cost_func(P, X):.4f}")
             self.P = self.train(X, P)
             print(f"Final cost: {self.cost_func(P, X):.4f}")
+            self.save_history(name)
             if name is not None:
                 self.save(name)
         else:
             self.P = self.load(name)
-
+            self.load_history(name)
 
     def initialize_weights(self, activation):
         P = [None] * (len(self.nodes) - 1)
@@ -60,55 +62,129 @@ class PDE_solver_NN_base(Activations):
         for t in pbar:
             try:
                 gradient = cost_func_grad(P, X)
-                for l in range(len(self.nodes) - 1):
-                    P[l] = P[l] - self.eta(t) * gradient[l]
+                for L in range(len(self.nodes) - 1):
+                    P[L] = P[L] - self.eta(t) * gradient[L]
                 cf = self.cost_func(P, X)
                 pbar.set_description(f"{cf:.10f}")
                 self.history[t] = cf
+
             except KeyboardInterrupt:
+                self.epochs = t
+                self.history = self.history[:t]
                 break
         return P
 
     def feed_forward(self, x, P):
         prev = x
-        for l in range(len(self.nodes) - 1):
+        for L in range(len(self.nodes) - 1):
             prev = np.concatenate((prev, np.ones((prev.shape[0], 1))), axis=1)
-            z = prev @ P[l]
+            z = prev @ P[L]
             a = self.act_func(z)
             prev = a
         return z
 
     def __call__(self, x, P):
+        """ Used to evaluate network """
         return self.feed_forward(x, P)
 
     def cost_func(self, P, X):
+        """
+        Calculates error as difference between lhs and rhs of PDE
+        returnes the normalized square error
+        """
         error = (self.lhs(X, P) - self.rhs(X, P)) ** 2
         return np.sum(error, axis=0) / error.shape[0]
 
     def lhs(self, X, P):
+        """
+        Left-hand side of differential equation,
+        must be implemented separately through inheritance
+
+        Should only take X and P as arguments,
+        and return the lhs evaluated with the trial function
+        Derivatives taken with autograd.elementwise_grad (ele_grad)
+
+        Example:
+        PDE: d^2/dx^2 u(x, t) = d/dt u(x, t)
+        lhs = ele_grad(ele_grad(trial, 0), 0)(x, t, X, P)
+        rhs = ele_grad(trial, 1)(x, t, X, P)
+
+        trial function enforces boundary/initial conditions.
+        Must be a function of the weights and biases of the network M,
+        as such: self(X, P), where X is the datapoints, and P the weights/biases
+        Simply pass the X and P from lhs/rhs to the trial func, and furhter on to self()
+        """
         raise NotImplementedError
 
     def rhs(self, X, P):
+        """
+        Right-hand side of differential equation,
+        must be implemented separately through inheritance
+        See lhs for more documentation
+        """
         raise NotImplementedError
 
     def trial(self, X, P):
+        """
+        Trial func is specific to PDE and boundary conditions,
+        must be implemented separately through inheritance
+        See lhs for more documentation
+        """
         raise NotImplementedError
 
-    def get_solution(self, *args):
-        return self.trial(*args, self.X, self.P)
+    def eta(self, epoch):
+        """
+        Learning schedule
+
+        Can be re-implemented to try different schedules
+        """
+        return self.eta0
+
+    def get_solution(self):
+        """
+        Return solution to PDE after network is trained
+        """
+        return self.trial(*self.X.T, self.X, self.P)
 
     def save(self, name):
+        """ Save weights and biases """
         name = f"{name}_{self.act_func.__name__}"
         for i in self.nodes[1:-1]:
             name += "_" + str(i)
+        print(f"Saving net {name}")
         np.save("./nets/" + name, np.asarray(self.P))
 
     def load(self, name):
+        """ Load weights and biases """
         name = f"{name}_{self.act_func.__name__}"
         for i in self.nodes[1:-1]:
             name += "_" + str(i)
+        print(f"Loading net {name}")
         fname = "./nets/" + name + ".npy"
         return list(np.load(fname, allow_pickle=True))
+
+    def save_history(self, name):
+        """ Save train history for later visulaization """
+        name = f"TH_{name}_{self.act_func.__name__}"
+        for i in self.nodes[1:-1]:
+            name += "_" + str(i)
+        print(f"Saving train history of {name}")
+        t = np.arange(self.epochs)
+        A = np.concatenate((t[:, None], self.history[:, None]), axis=1)
+        np.save("./nets/" + name, A)
+
+    def load_history(self, name):
+        """ Reload train history """
+        name = f"TH_{name}_{self.act_func.__name__}"
+        for i in self.nodes[1:-1]:
+            name += "_" + str(i)
+        print(f"Loading train history of {name}")
+        A = np.load("./nets/" + name + ".npy", allow_pickle=True)
+        self.t, self.history = A.T
+        self.epochs = self.t[-1]
+
+
+
 
 
 
