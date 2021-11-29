d25ec11e2af854db37ec460338a4b8b94a135ef3
haakooto <hakonolav.torvik@gmail.com>
2021-11-29 21:01:07 +0100

    do parameter optimization

--- Project3/code/omtimizer_test.py
+++ Project3/code/omtimizer_test.py
@@ -18,33 +18,35 @@ class Diffusion(PDE_solver_NN_base):
         return np.sin(np.pi * x[:, None]) * (1 + t[:, None] * self(X, P))
 
     def eta(self, epoch):
-        return self.eta0 * (1 - 0 * epoch / self.epochs)
+        if epoch > 100:
+            r = self.history[epoch - 3] / self.history[epoch - 1]
+            if r > 1:
+                return self.eta0 * r ** (30 * (1 - epoch / self.epochs))
+            return self.eta0
+        return self.eta0
 
 
 def main():
-    dx = 0.01
-    dt = 0.01
-    T = 5
-    nx = 1 / dx + 1
-    nt = T / dt + 1
-    x = np.linspace(0, 1, int(nx))
-    t = np.linspace(0, T, int(nt))
+    x = np.linspace(0, 1, 21)
+    t = np.linspace(0, 5, 201)
     x, t = np.meshgrid(x, t)
     X = np.concatenate((x.reshape(-1, 1), t.reshape(-1, 1)), axis=1)
+    print(X.shape)
 
-    Solver = Diffusion(X, 
+    Solver = Diffusion(X.shape[1],
                        nodes=[10, 40, 20, 10], 
                        activation="abs",
                        alpha=-1,
-                       epochs=500, 
-                       eta0=0.000001,
+                       epochs=100_000, 
+                    #    epochs=500, 
+                       eta0=0.000004,
                        lmb=0, 
                        gamma=0.9, 
                        load=False, 
-                       name=None, 
+                       name=None,
                        seed=2021,
                        )
-    Solver.train()
+    Solver.train(X)
     solution = Solver.get_solution()  # trial function solution
     solution = solution.reshape(x.shape)
     raw_solution = Solver(X, Solver.P)  # the way the net looks like without trial function
@@ -53,11 +55,11 @@ def main():
     fig = go.Figure(data=[go.Scatter(x=Solver.t[10:], y=Solver.history[10:], mode="lines")])
     fig.show()
 
-    # fig = go.Figure(data=[go.Surface(x=x, y=t, z=solution)])
-    # fig.show()
+    fig = go.Figure(data=[go.Surface(x=x, y=t, z=solution)])
+    fig.show()
 
-    # fig = go.Figure(data=[go.Surface(x=x, y=t, z=raw_solution)])
-    # fig.show()
+    fig = go.Figure(data=[go.Surface(x=x, y=t, z=raw_solution)])
+    fig.show()
 
 if __name__ == "__main__":
     main()
