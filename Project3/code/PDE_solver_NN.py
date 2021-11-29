64dd9514c64435019b2816d53c1435c2f4f9a04c
haakooto <hakonolav.torvik@gmail.com>
2021-11-29 18:17:43 +0100

    add optimizer, make relu-param alpha changable, add abs as activation function

--- Project3/code/PDE_solver_NN.py
+++ Project3/code/PDE_solver_NN.py
@@ -8,27 +8,54 @@ class Activations:
     def sigmoid(self, x):
         return 1 / (1 - np.exp(-x))
 
-    def ReLU(self, x):
-        return np.where(x > 0, x, 0)
+    def abs(self, x):
+        return np.abs(x)
 
     def PReLU(self, x):
         return np.where(x > 0, x, self.alpha * x)
 
 
+class Optimizer:
+    def __init__(self, gamma=0, layer_sizes=None):
+        self.g = min(1, max(gamma, 0))  # ensure between 0 and 1
+        self.prev_v = [np.zeros((i + 1, 1)) + 1e-6 for i in layer_sizes]
+
+    def __call__(self, eta_grad, layer=0):
+        v = self.g * self.prev_v[layer] + eta_grad
+        self.prev_v[layer] = v
+        return v
+
+
 class PDE_solver_NN_base(Activations):
-    def __init__(self, X, epochs, nodes, eta0, activation="relu", load=False, name=None, seed=21345):
-        self.alpha = 0.01  # for PReLU (leaky relu)
+    def __init__(self, 
+                 X, 
+                 nodes=[10,],          # nodes in each hidden layer
+                 activation="relu",    # actuvation function
+                 alpha=0,
+                 epochs=100,           # training epochs
+                 eta0=0.001,           # initial learning rate. Implement eta method to change schedule. is constant by default
+                 lmb=0,                # L2 regulizer
+                 gamma=0,              # momentum
+                 load=False,           # Load weights from file instead of training
+                 name=None,            # name of file (nickname) 
+                 seed=21345,           # random seed for weight initialization
+                 ):
         self.nodes = np.array([X.shape[1], *nodes, 1])
         self.rng = np.random.default_rng(seed)
 
         self.epochs = epochs
         self.eta0 = eta0
+        self.lmb = lmb
+        self.Optim = Optimizer(gamma, self.nodes)
 
+        
+        self.alpha = alpha  # linearity for activations below 0 (leaky relu factor)
         activations = {"sigmoid": self.sigmoid,
-                       "relu": self.ReLU,
-                       "prelu": self.PReLU,
+                       "relu": self.PReLU,
+                       "abs": self.abs
                        }
         self.act_func = activations[activation]
+
         self.X = X
         self.t = np.arange(self.epochs)
 
@@ -38,8 +65,8 @@ class PDE_solver_NN_base(Activations):
             print(f"Initial cost: {self.cost_func(P, X):.4f}")
             self.P = self.train(X, P)
             print(f"Final cost: {self.cost_func(P, X):.4f}")
-            self.save_history(name)
             if name is not None:
+                self.save_history(name)
                 self.save(name)
         else:
             self.P = self.load(name)
@@ -50,7 +77,7 @@ class PDE_solver_NN_base(Activations):
         for i in range(1, len(self.nodes)):
             n = self.nodes[i - 1]
             m = self.nodes[i]
-            s = np.sqrt(2 / n)
+            s = np.sqrt(6 / (1 + self.alpha**2) / (n + m))
             P[i - 1] = self.rng.normal(0, s, (n + 1, m))
             P[i - 1][-1, :] = 0.01
         return P
@@ -63,7 +90,10 @@ class PDE_solver_NN_base(Activations):
             try:
                 gradient = cost_func_grad(P, X)
                 for L in range(len(self.nodes) - 1):
-                    P[L] = P[L] - self.eta(t) * gradient[L]
+                    Lgrad = gradient[L] + self.lmb * P[L]
+                    update = self.Optim(self.eta(t) * Lgrad, L)
+                    # print(update.shape, np.max(update))
+                    P[L] = P[L] - update
                 cf = self.cost_func(P, X)
                 pbar.set_description(f"{cf:.10f}")
                 self.history[t] = cf
